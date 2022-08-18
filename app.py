f5ed84f7b2b5a1b5f52b40fd5e2b3bc0738076db
Kovsh <Vladislav.Koshevoy2@gmail.com>
2022-08-18 19:45:21 +0300

    Server started

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///passgen.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Passwords(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, default='Null')
+
+    def __repr__(self):
+        return '<Passwords %r>' % self.id
+
+@app.route('/')
+def main():
+    return {"passwords": ["pass1", "pass2", "pass3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
