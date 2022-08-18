5bdbf3a2826d6d6df26731b14980dc9c054bfa2f
Kovsh <Vladislav.Koshevoy2@gmail.com>
2022-08-18 21:31:32 +0300

    Minor update

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///passgen.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 class Passwords(db.Model):
@@ -15,9 +17,11 @@ class Passwords(db.Model):
     def __repr__(self):
         return '<Passwords %r>' % self.id
 
+
 @app.route('/')
 def main():
     return {"passwords": ["pass1", "pass2", "pass3"]}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
