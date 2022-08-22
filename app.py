f20c4ca7e65ff65620a9c00d3fd898eaf5542290
Kovsh <Vladislav.Koshevoy2@gmail.com>
2022-08-22 20:35:49 +0300

    Fully workable back-end

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from random import randint
+
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
@@ -12,15 +14,66 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 class Passwords(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, default='Null')
+    user_id = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<Passwords %r>' % self.id
+        return '<Password %r>' % self.id
 
 
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def main():
-    return {"passwords": ["pass1", "pass2", "pass3"]}
+    if request.method == 'POST':
+        content = request.json
+
+        totArr = checker(content)
+        passwd = genPass(totArr, content['amount'])
+
+        record = Passwords(password=passwd)
+
+        try:
+            db.session.add(record)
+            db.session.commit()
+            return f'{passwd}   JSON: {content}'
+
+        except:
+            return 'An error while recording'
+
+    else:
+        return "What r u doing here?"
+
+
+def checker(content):
+    uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lowercase = 'abcdefghijklmnopqrstuvwxyz'
+    digits = '0123456789'
+    symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    totArr = ''
+
+    if content['ll']:
+        totArr += uppercase
+
+    if content['sl']:
+        totArr += lowercase
+
+    if content['num']:
+        totArr += digits
+
+    if content['sym']:
+        totArr += symbols
+
+    return totArr
+
+
+def genPass(totArr, amount):
+    passwd = ''
+
+    if amount == '':
+        return "Amount is empty!"
+
+    for i in range(int(amount)):
+        passwd += totArr[randint(0, len(totArr) - 1)]
+
+    return passwd
 
 
 if __name__ == "__main__":
